8988f7de6e9722004d0b40ebeb3646389f645d9f
mqgg5630-cyber <308188665+mqgg5630-cyber@users.noreply.github.com>
2026-09-05 08:14:51 +0000

    Add GPU allow_growth and batch size acceleration to Attention and LSTM prediction scripts
    Co-authored-by: arena-agent <297053741+arena-agent@users.noreply.github.com>

--- script/prediction_attention.py
+++ script/prediction_attention.py
@@ -1,13 +1,22 @@
 # -*- coding:utf-8 -*-
 
 ## usage python prediction_attention.py bact.txt att_bact.txt
+import os
+import tensorflow as tf
+from keras.backend.tensorflow_backend import set_session
 from keras.models import load_model
 from numpy import loadtxt, savetxt
 from Attention import Attention_layer
 from sys import argv
 
+# 动态申请 GPU 显存, 防止占满显存
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+set_session(tf.Session(config=config))
+
 model = load_model('../Models/att.h5', custom_objects={'Attention_layer': Attention_layer})
 x = loadtxt(argv[1], delimiter=",")
 
-preds = model.predict(x)
+batch_size = int(os.environ.get("TF_PREDICT_BATCH_SIZE", "2048"))
+preds = model.predict(x, batch_size=batch_size)
 savetxt(argv[2], preds, fmt="%.8f", delimiter=",")
--- script/prediction_lstm.py
+++ script/prediction_lstm.py
@@ -1,12 +1,21 @@
 # -*- coding:utf-8 -*-
 
 ## usage python prediction_lstm.py sequence_after_format.txt lstm_bact.txt
+import os
+import tensorflow as tf
+from keras.backend.tensorflow_backend import set_session
 from keras.models import load_model
 from numpy import loadtxt, savetxt
 from sys import argv
 
+# 动态申请 GPU 显存, 防止占满显存
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+set_session(tf.Session(config=config))
+
 model = load_model('../Models/lstm.h5')
 x = loadtxt(argv[1], delimiter=",")
 
-preds = model.predict(x)
+batch_size = int(os.environ.get("TF_PREDICT_BATCH_SIZE", "2048"))
+preds = model.predict(x, batch_size=batch_size)
 savetxt(argv[2], preds, fmt="%.8f", delimiter=",")
